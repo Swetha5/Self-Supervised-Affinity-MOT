625acb7a1cfc60a80aedd105075fcc4f5ca326c6
BravoCharlie-ai <52042236+BravoCharlie-ai@users.noreply.github.com>
2021-09-13 12:03:08 -0400

    Add files via upload

---
+++ JRDB/JRDB_Self_supervised/eval_dataset.py
@@ -0,0 +1,142 @@
+import json
+import numpy as np
+from torch.utils.data import DataLoader, Dataset
+from functools import partial
+import torch
+from eval_dataset_preprocess import get_pointcloud,create_frame_detections,get_frame_det_info,get_train_names,get_test_names,get_val_names
+
+class TestSequenceDataset(object):
+    def __init__(self, root_dir, label_dir, fix_iou=0.2, fix_count=2, gt_ratio=0, sample_max_len=2):
+        
+        self.root_dir = root_dir
+        self.sample_max_len = sample_max_len
+        self.label_dir = label_dir
+
+        #self.test = False
+        #self.val_seqs=['clark-center-2019-02-28_1']
+        #self.val_seqs=['bytes-cafe-2019-02-07_0','clark-center-2019-02-28_1']
+        self.val_seqs=get_val_names()
+        #self.val_seqs=get_train_names()
+        self.get_pointcloud = get_pointcloud
+        self.metas = self._generate_meta_seq()
+
+    def __len__(self):
+        return len(self.metas)
+
+    def __getitem__(self, idx):
+        return self.metas[idx]
+    
+    def _generate_meta_seq(self):
+        #root_dir='train_dataset/pointclouds_pt/'
+        metas = []
+        for seq_id in self.val_seqs:
+            name=seq_id
+            #fname='/home/c3-0/datasets/JRDB/cvgl/group/jrdb/data/test_dataset/detections/detections_3d/'+seq_id+'.json'
+            
+            #fname='/home/c3-0/datasets/JRDB/cvgl/group/jrdb/data/train_dataset/labels/labels_3d/'+seq_id+'.json'
+            fname=self.label_dir+seq_id+'.json'
+            with open(fname) as json_file:
+                orig_labels = json.load(json_file)
+
+            
+            #labels=orig_labels['detections']
+            #frame_id=list(sorted(orig_labels['detections'].keys()))
+
+            labels=orig_labels['labels']
+            frame_id=list(sorted(orig_labels['labels'].keys()))
+
+            det_seq=[]
+            for i in range(0, len(frame_id)-1):
+                #Each test sample must contains two frames
+                det_frames=[]
+
+                #frame_1
+                frame_1={}
+                frame_1['seq_id']=seq_id
+                frame_1['frame_id']=frame_id[i]
+                frame_1['objects']=labels[frame_id[i]]
+                frame_1['detection']=create_frame_detections(frame_1)
+                det_frames.append(frame_1)
+
+
+                #frame_2
+                frame_2={}
+                frame_2['seq_id']=seq_id
+                frame_2['frame_id']=frame_id[i+1]
+                frame_2['objects']=labels[frame_id[i+1]]
+                frame_2['detection']=create_frame_detections(frame_2)
+                det_frames.append(frame_2)
+
+                #test sample of combined frames
+                det_seq.append(det_frames)
+
+            metas.append(TestSequence(name, self.root_dir,det_seq, self.get_pointcloud,interval=2))
+        return metas    
+
+
+class TestSequence(Dataset):
+    def __init__(self,name, root_dir, det_frames, get_pointcloud_partial, interval=2):
+        self.det_frames = det_frames
+        self.interval = interval
+        self.root_dir = root_dir
+        self.metass= det_frames
+        self.idx = 0
+        self.seq_len = len(det_frames) + interval -1
+        self.name = name
+        self.get_pointcloud = get_pointcloud_partial
+        '''
+
+        if transform == None:
+            self.transform = transforms.Compose([transforms.ToTensor()])
+        else:
+            self.transform = transform
+        assert len(det_frames) == len(det_frames)
+        '''
+
+    def __getitem__(self, idx):
+        return self._generate_lidar(idx)
+
+    def __len__(self):
+        return len(self.metass)
+    
+    def _generate_lidar(self, idx):
+        frames = self.metass[idx]
+        det_imgs = []
+        det_split = []
+        dets = []
+        det_info = get_frame_det_info()
+        first_flag = 0
+        for frame in frames:
+            seq_id=frame['seq_id']
+            frame_id=frame['frame_id'].split('.')[0]
+
+            point_cloud=get_pointcloud(frame['detection'],seq_id,frame_id,self.root_dir)
+
+
+            det_num = frame['detection']['bbox'].shape[0]
+            #det_num=min(det_num,80)
+            dets.append(frame['detection'])
+
+            det_split.append(det_num)
+            det_info['loc'].append(torch.Tensor(frame['detection']['location']))
+            det_info['rot'].append(torch.Tensor(frame['detection']['rotation_y']))
+            det_info['dim'].append(torch.Tensor(frame['detection']['dimensions']))
+            det_info['points'].append(torch.Tensor(point_cloud['points']))
+            det_info['points_split'].append(torch.Tensor(point_cloud['points_split'])[first_flag:])
+            det_info['info_id'].append(seq_id+'/'+frame_id)
+
+            if first_flag == 0:
+                first_flag += 1
+
+        det_imgs = []
+        det_info['loc'] = torch.cat(det_info['loc'], dim=0)
+        det_info['rot'] = torch.cat(det_info['rot'], dim=0)
+        det_info['dim'] = torch.cat(det_info['dim'], dim=0)
+        det_info['points'] = torch.cat(det_info['points'], dim=0)
+        start = 0
+        for i in range(len(det_info['points_split'])):
+            det_info['points_split'][i] += start
+            start = det_info['points_split'][i][-1]
+        det_info['points_split'] = torch.cat(det_info['points_split'], dim=0)
+        return det_imgs, det_info, dets, det_split
+
---
+++ JRDB/JRDB_Self_supervised/eval_seq-ss.py
@@ -0,0 +1,184 @@
+import argparse
+import logging
+import os
+import pprint
+import time
+
+import torch
+import torch.backends.cudnn as cudnn
+import torch.optim
+import yaml
+from easydict import EasyDict
+#from kitti_devkit.evaluate_tracking import evaluate
+from torch.utils.data import DataLoader
+from tracking_model import TrackingModule
+from SAMOT import build_model
+#from utils.build_util import build_augmentation, build_dataset
+from data_util import write_kitti_result
+from train_util import AverageMeter, create_logger
+from convert_to_JRDB import files_convert_to_JRDB_formate
+from tracking_eval.eval_mot import evaluate
+
+parser = argparse.ArgumentParser(description='PyTorch mmMOT Evaluation')
+#parser.add_argument('--config', default='config.yaml')
+#parser.add_argument('--load-path', default='new_weights/2D_next_random/ssj300_0712_50.0.pth', type=str)
+#parser.add_argument('--result-path', default="/home/aakash/Downloads/SAMOT_gpu/evaluation_results/random_next_2D_more_experiments/experiment_1/epoch_40", type=str)
+#parser.add_argument('--result-path', default="", type=str)
+parser.add_argument('--load_model_dir', default="", type=str)
+parser.add_argument('--recover', action='store_true')
+#parser.add_argument('-e', '--evaluate', action='store_true')
+parser.add_argument('--result_sha', default='last')
+parser.add_argument('--memory', action='store_true')
+
+global args, config, best_mota
+args = parser.parse_args()
+
+with open('config.yaml') as f:
+	config = yaml.load(f, Loader=yaml.FullLoader)
+
+config = EasyDict(config['common'])
+args.result_path=config.val_prediction_dir
+
+
+
+
+
+from eval_dataset import TestSequenceDataset
+def main():
+	root_dir='/home/c3-0/datasets/JRDB/MOT_output_pt/sequences/'
+	val_dataset=TestSequenceDataset(config.root_dir+'sequences/',label_dir=config.train_label_dir)
+
+
+
+
+	#load_model_dir='/home/aa809504/SAMOT_gpu/new_weights/Pedestrian/experiment_1/ssj300_0712_epoch_45.0_Loss_ 0.2763.pth'
+	load_model_dir=config.load_weights
+	cuda_device='True'
+	sst_net = build_model('train',cuda_device)
+
+	if cuda_device:
+		model=sst_net.cuda()
+	else:
+		model=sst_net
+
+	model.load_state_dict(torch.load(load_model_dir))
+	tracking_module = TrackingModule(model, None, None, '3D')
+
+	cudnn.benchmark = True
+
+	#save_path = '/home/aakash/Downloads/SAMOT_gpu'
+
+
+
+	#logger = create_logger('global_logger', save_path + '/eval_log.txt')
+	#logger.info('args: {}'.format(pprint.pformat(args)))
+	#logger.info('config: {}'.format(pprint.pformat(config)))
+	validate(val_dataset, tracking_module, 'last', part='val')
+
+def validate(val_loader,
+			 tracking_module,
+			 step,
+			 part='train',
+			 fusion_list=None,
+			 fuse_prob=False):
+	prec = AverageMeter(0)
+	rec = AverageMeter(0)
+	mota = AverageMeter(0)
+	motp = AverageMeter(0)
+
+	logger = logging.getLogger('global_logger')
+	for i, (sequence) in enumerate(val_loader):
+		logger.info('Test: [{}/{}]\tSequence ID: KITTI-{}'.format(
+			i, len(val_loader), sequence.name))
+		seq_loader = DataLoader(
+			sequence,
+			batch_size=config.batch_size,
+			shuffle=False,
+			num_workers=config.workers,
+			pin_memory=True)
+		if len(seq_loader) == 0:
+			tracking_module.eval()
+			logger.info('Empty Sequence ID: KITTI-{}, skip'.format(
+				sequence.name))
+		else:
+			#if args.memory:
+			#    seq_prec, seq_rec, seq_mota, seq_motp = validate_mem_seq(
+			#        seq_loader, tracking_module)
+			#else:
+			print('sequence number ', i)
+			print('validate_sequence ',sequence.name)
+			print('sequence length ',len(sequence))
+			seq_prec, seq_rec, seq_mota, seq_motp = validate_seq(
+				seq_loader, tracking_module)
+			print('seq_mota',seq_mota)
+			prec.update(seq_prec, 1)
+			rec.update(seq_rec, 1)
+			mota.update(seq_mota, 1)
+			motp.update(seq_motp, 1)
+
+		write_kitti_result(
+			args.result_path,
+			sequence.name,
+			step,
+			tracking_module.frames_id,
+			tracking_module.frames_det,
+			part=part)
+		read_dir=args.result_path+'/'+step+'/'+part+'/'+sequence.name+'.txt'
+		write_dir=args.result_path+'/'+'JRDB_formate'
+		if not os.path.exists(write_dir):
+			os.mkdir(write_dir)
+		files_convert_to_JRDB_formate(read_dir,write_dir+'/'+sequence.name+'.txt')
+
+
+	total_num = torch.Tensor([prec.count])
+	logger.info(
+		'* Prec: {:.3f}\tRec: {:.3f}\tMOTA: {:.3f}\tMOTP: {:.3f}\ttotal_num={}'
+		.format(prec.avg, rec.avg, mota.avg, motp.avg, total_num.item()))
+	#MOTA, MOTP, recall, prec, F1, fp, fn, id_switches = evaluate(
+	#    step, args.result_path, part=part)
+
+	tracking_module.train()
+	#return MOTA, MOTP, recall, prec, F1, fp, fn, id_switches
+	print(evaluate(groundtruths=config.val_label_dir,tests=config.val_prediction_dir))
+
+
+def validate_seq(val_loader,
+				 tracking_module,
+				 fusion_list=None,
+				 fuse_prob=False):
+	batch_time = AverageMeter(0)
+
+	# switch to evaluate mode
+	tracking_module.eval()
+
+	logger = logging.getLogger('global_logger')
+	end = time.time()
+
+	with torch.no_grad():
+		for i, (input, det_info, dets, det_split) in enumerate(val_loader):
+			#input = input.cuda()
+			'''
+			if len(det_info) > 0:
+				for k, v in det_info.items():
+					det_info[k] = det_info[k].cuda() if not isinstance(
+						det_info[k], list) else det_info[k]'''
+
+			# compute output
+			aligned_ids, aligned_dets, frame_start = tracking_module.predict(
+				input, det_info, dets, det_split)
+			#print(aligned_ids)
+			if i%100==0:
+				print('samples_passed',i)
+
+			batch_time.update(time.time() - end)
+			end = time.time()
+			if i % config.print_freq == 0:
+				logger.info('Test Frame: [{0}/{1}]\tTime {batch_time.val:.3f}'
+							'({batch_time.avg:.3f})'.format(
+								i, len(val_loader), batch_time=batch_time))
+
+	return 0, 0, 0, 0
+
+
+if __name__ == '__main__':
+	main()
